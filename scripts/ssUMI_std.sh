48a1f1a676cbf4c86430cd88b687372ccc09d20b
Xuan Lin <54858705+xlin-1@users.noreply.github.com>
2023-06-16 12:33:48 -0700

    Update ssUMI_std.sh

--- scripts/ssUMI_std.sh
+++ scripts/ssUMI_std.sh
@@ -11,7 +11,7 @@
 ### Description ----------------------------------------------------------------
 
 USAGE="
--- longread_umi ssUMI_std: Generate UMI consensus sequences of full-length 16S rRNA from Q20+ Nanopore data in standard mode
+-- longread_umi ssumi_std: Generate UMI consensus sequences of full-length 16S rRNA from Q20+ Nanopore data in standard mode
    
 usage: $(basename "$0" .sh) [-h] [-w string] (-d file -v value -o dir -s value) 
 (-e value -m value -M value -f string -F string -r string -R string )
